a133c6e0c04e2db6cd6ee21cb5e547207bccff84
Sumit Agrawal <sumitagrawal4548@gmail.com>
2018-04-28 22:01:31 +0530

    Changes - Package Structure

--- src/main/java/com/eplvc/eplvcservice/capturedphotoes/CapturedStage.java
+++
@@ -1,7 +0,0 @@
-package com.eplvc.eplvcservice.capturedphotoes;
-
-public enum CapturedStage {
-	CAMERA_PERMISSION,
-	POLICY_NAME,
-	POLICY_DETAILS;
-}
--- src/main/java/com/eplvc/eplvcservice/capturedphotoes/EplvcImageRepo.java
+++
@@ -1,7 +0,0 @@
-package com.eplvc.eplvcservice.capturedphotoes;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface EplvcImageRepo extends CrudRepository<EplvcCapturedPhotos, Long>{
-
-}
